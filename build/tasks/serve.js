88382bf5808c7a8c450eb4292ed089faf47c9782
SidHarder <softwarenavigator@gmail.com>
2016-01-07 14:49:13 -0700

    change port to 80

--- build/tasks/serve.js
+++ build/tasks/serve.js
@@ -1,14 +1,11 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 
-// this task utilizes the browsersync plugin
-// to create a dev server instance
-// at http://localhost:9000
 gulp.task('serve', ['build'], function(done) {
   browserSync({
     online: false,
     open: false,
-    port: 9000,
+    port: 80,
     server: {
       baseDir: ['.'],
       middleware: function(req, res, next) {
